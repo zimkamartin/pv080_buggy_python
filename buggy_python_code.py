ef5648b2d710c8abde9d3ccbb2374112274800b9
zimkamartin <martin.zimka@gmail.com>
2022-04-24 15:17:06 +0200

    removed import of cPickle

--- buggy_python_code.py
+++ buggy_python_code.py
@@ -1,7 +1,6 @@
 # this comment is here only bcs of commit
 # contains bunch of buggy examples
 # taken from https://hackernoon.com/10-common-security-gotchas-in-python-and-how-to-avoid-them-e19fbe265e03
-import cPickle
 import subprocess
 import base64
 import subprocess
